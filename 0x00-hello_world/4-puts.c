7689b8aa007461aa098e70e80edf515f81ebd98d
Omoluabi45 <agboolaluqman@gmail.com>
2023-02-16 07:20:16 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+/**
+ * main - A program that print line with puts functions
+ * return: 0 (Done)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
